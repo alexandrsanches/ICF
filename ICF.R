82f79dad142ca0d2f4f95ae15edba9f0adb148b9
Henrique Andrade <henrique.coelho@gmail.com>
2020-06-04 17:25:36 -0300

    [add-change] Início da coleta de dados e inclusão de tabela no Readme

--- ICF.R
+++ ICF.R
@@ -1,39 +1,95 @@
 #### Define working directory ####
 
-if (Sys.info()["sysname"] == "Linux") {
-    setwd("~/Desenvolvimento/SPE/Indicador de Condições Financeiras")
-} else if (Sys.info()["sysname"] == "Darwin"){
-    setwd("~/Documents/GitHub/Indicador de Condições Financeiras")
-} else {
-    setwd("~/Documents/Indicador de Condições Financeiras")
-}
+switch (Sys.info()["sysname"],
+    Linux = setwd("~/Desenvolvimento/SPE/Indicador de Condições Financeiras"),
+    Darwin = setwd("~/Documents/GitHub/Indicador de Condições Financeiras"),
+    Windows = setwd("~/Documents/Indicador de Condições Financeiras")
+)
 
 #### Load packages and functions #### 
 
-suppressPackageStartupMessages({
-    library(kableExtra)
-    library(ggrepel)
-    library(tidyverse)
-    library(tidycovid19) # Pacote do GitHub - https://github.com/joachim-gassen/tidycovid19
-    library(tsibble)
-    library(reshape2)
-    library(patchwork)
-    library(gghighlight)
-    library(zoo)
-    library(knitr) 
-    library(rmarkdown)
-    library(openxlsx)
-    library(tseries)
-    library(wesanderson)
-    library(lubridate)
-    library(HDeconometrics) # Pacote do GitHub - https://github.com/gabrielrvsc/HDeconometrics
-})
+suppressPackageStartupMessages(
+    library(xts)
+)
 
 source("Scripts/Functions.R", encoding = "utf8")
 
 #### Import data ####
 
+#| Grupos | Nomes               | Séries                                                                       | Pesos |
+#|--------|---------------------|------------------------------------------------------------------------------|-------|
+#| 1      | Juros Brasil        | Taxas de juros (Swap Pré-DI) de 1 e 5 anos                                   |  0,34 |
+#| 2      | Juros exterior      | Taxas de juros dos EUA, Reino Unido, Alemanha e Japão (3 meses, 2 e 10 anos) |  0,33 |
+#| 3      | Risco               | CDS Brasil (5 anos) e VIX                                                    |  0,18 |
+#| 4      | Moedas              | US dollar indexes (desenvolvidos, emergentes), taxa de câmbio (R$/US$)       |  0,20 |
+#| 5      | Petróleo            | Cotações em US$ do barril de petróleo (WTI e Brent)                          |  0,23 |
+#| 6      | Commodities         | Índices de commodities CRB (foodstuffs, metals)                              | -0,13 |
+#| 7      | Mercado de capitais | Índices de ações MSCI (desenvolvidos, emergentes) e Ibovespa                 | -0,15 |
 
+| Séries                                        | Status | Fonte |
+|-----------------------------------------------|--------|-------|
+| Swap Pré-DI 1 ano                             |        |       |
+| Swap Pré-DI 5 anos                            |        |       |
+| Juros EUA 3 meses                             |        |       |
+| Juros EUA 2 anos                              |        |       |
+| Juros EUA 10 anos                             |        |       |
+| Juros Reino Unido 3 meses                     |        |       |
+| Juros Reino Unido 2 anos                      |        |       |
+| Juros Reino Unido 10 anos                     |        |       |
+| Juros Alemanha 3 meses                        |        |       |
+| Juros Alemanha 2 anos                         |        |       |
+| Juros Alemanha 10 anos                        |        |       |
+| Juros Japão 3 meses                           |        |       |
+| Juros Japão 2 anos                            |        |       |
+| Juros Japão 10 anos                           |        |       |
+| CDS Brasil (5 anos)                           |        |       |
+| VIX                                           |        |       |
+| US dollar indexes (desenvolvidos)             |        |       |
+| US dollar indexes (emergentes)                |        |       |
+| Taxa de câmbio (R$/US$)                       |        |       |
+| Cotações em US$ do barril de petróleo (WTI)   |        |       |
+| Cotações em US$ do barril de petróleo (Brent) |        |       |
+| Índices de commodities CRB (foodstuffs)       |        |       |
+| Índices de commodities CRB (metals)           |        |       |
+| Índices de ações MSCI (desenvolvidos)         |        |       |
+| Índices de ações MSCI (emergentes)            |        |       |
+| Ibovespa                                      |        |       |
+
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#### Data transformation ####
+
+#### Index construction ####
 
 #### Plots #####
 
@@ -49,16 +105,16 @@ save.image(file = "Dados/Data & Functions.RData")
 
 #### Render R Markdown ####
 
-estilo_markdown <- "Scripts/Markdown/Estilos/Architect/architect.css" 
-cabecalho_rodape_markdown <- includes(in_header = "Scripts/Markdown/Cabeçalhos/cabecalhoCOVID19.html",
-                                      after_body = "Scripts/Markdown/Rodapés/rodape.html") 
+markdown_custom_css <- "Scripts/Markdown/Estilos/Architect/architect.css" 
+markdown_header_footer <- includes(in_header = "Scripts/Markdown/Cabeçalhos/cabecalhoCOVID19.html",
+                                   after_body = "Scripts/Markdown/Rodapés/rodape.html") 
 
 render(input = "Scripts/Final (HTML).Rmd",
        output_file = "Resultados/Final (HTML).html",
        output_format = html_document(fig_width = 12,
                                      fig_height = 8,
-                                     includes = cabecalho_rodape_markdown, 
-                                     css = estilo_markdown),
+                                     includes = markdown_header_footer, 
+                                     css = markdown_custom_css),
        knit_root_dir = getwd(),
        encoding = "UTF-8",
        quiet = TRUE)
--- README.md
+++ README.md
@@ -31,3 +31,34 @@
 
 - Ações MSCI (desenvolvidos e emergentes)
 - Ibovespa
+
+## Dados utilizados na nota do Banco Central
+
+| Grupos | Nomes               | Séries                                        | Pesos |
+| ------ | ------------------- | --------------------------------------------- | ----- |
+| 1      | Juros Brasil        | Swap Pré-DI 1 ano                             | 0,34  |
+|        |                     | Swap Pré-DI 5 anos                            |       |
+| 2      | Juros exterior      | EUA 3 meses                                   | 0,33  |
+|        |                     | EUA 2 anos                                    |       |
+|        |                     | EUA 10 anos                                   |       |
+|        |                     | Reino Unido 3 meses                           |       |
+|        |                     | Reino Unido 2 anos                            |       |
+|        |                     | Reino Unido 10 anos                           |       |
+|        |                     | Alemanha 3 meses                              |       |
+|        |                     | Alemanha 2 anos                               |       |
+|        |                     | Alemanha 10 anos                              |       |
+|        |                     | Japão 3 meses                                 |       |
+|        |                     | Japão 2 anos                                  |       |
+|        |                     | Japão 10 anos                                 |       |
+| 3      | Risco               | CDS Brasil (5 anos)                           | 0,18  |
+|        |                     | VIX                                           |       |
+| 4      | Moedas              | US dollar indexes (desenvolvidos)             | 0,20  |
+|        |                     | US dollar indexes (emergentes)                |       |
+|        |                     | Taxa de câmbio (R\$/US\$)                     |       |
+| 5      | Petróleo            | Cotações em US$ do barril de petróleo (WTI)   | 0,23  |
+|        |                     | Cotações em US$ do barril de petróleo (Brent) |       |
+| 6      | Commodities         | Índices de commodities CRB (foodstuffs)       | -0,13 |
+|        |                     | Índices de commodities CRB (metals)           |       |
+| 7      | Mercado de capitais | Índices de ações MSCI (desenvolvidos)         | -0,15 |
+|        |                     | Índices de ações MSCI (emergentes)            |       |
+|        |                     | Ibovespa                                      |       |
