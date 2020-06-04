9d71f7445444f204331a7d4e37f074a1691ddcc5
Henrique Andrade <henrique.coelho@gmail.com>
2020-06-04 20:43:49 -0300

    [add] Transformação em xts e criação de data frame único

--- README.md
+++ README.md
@@ -39,7 +39,7 @@
 | 1      | Juros Brasil        | Taxas de juros (Swap Pré-DI) de 1 e 5 anos                                   |  0,34 |
 | 2      | Juros exterior      | Taxas de juros dos EUA, Reino Unido, Alemanha e Japão (3 meses, 2 e 10 anos) |  0,33 |
 | 3      | Risco               | CDS Brasil (5 anos) e VIX                                                    |  0,18 |
-| 4      | Moedas              | US dollar indexes (desenvolvidos, emergentes), taxa de câmbio (R$/US$)       |  0,20 |
+| 4      | Moedas              | US dollar indexes (desenvolvidos, emergentes), taxa de câmbio (R\$/US\$)       |  0,20 |
 | 5      | Petróleo            | Cotações em US$ do barril de petróleo (WTI e Brent)                          |  0,23 |
 | 6      | Commodities         | Índices de commodities CRB (foodstuffs, metals)                              | -0,13 |
 | 7      | Mercado de capitais | Índices de ações MSCI (desenvolvidos, emergentes) e Ibovespa                 | -0,15 |
@@ -50,9 +50,9 @@
 |-----------------------------------------------|--------|-------|
 | Swap Pré-DI 1 ano                             | ok!    | LCA (tem no Bacen: BCB7806) |
 | Swap Pré-DI 5 anos                            |        |       |
-| Juros EUA 3 meses                             |        |       |
-| Juros EUA 2 anos                              |        |       |
-| Juros EUA 10 anos                             |        |       |
+| Juros EUA 3 meses                             | ok! | LCA |
+| Juros EUA 2 anos                              | ok! | LCA |
+| Juros EUA 10 anos                             | ok! | LCA |
 | Juros Reino Unido 3 meses                     |        |       |
 | Juros Reino Unido 2 anos                      |        |       |
 | Juros Reino Unido 10 anos                     |        |       |
@@ -66,11 +66,11 @@
 | VIX                                           | ok! | LCA |
 | US dollar indexes (desenvolvidos)             |        |       |
 | US dollar indexes (emergentes)                |        |       |
-| Taxa de câmbio (R\$/US\$)                     |        |       |
+| Taxa de câmbio (R\$/US\$)                     | ok! | LCA |
 | Cotações em US$ do barril de petróleo (WTI)   | ok! | LCA |
 | Cotações em US$ do barril de petróleo (Brent) | ok! | LCA |
 | Índices de commodities CRB (foodstuffs)       | ok! | LCA |
 | Índices de commodities CRB (metals)           | ok! | LCA |
 | Índices de ações MSCI (desenvolvidos)         |        |       |
 | Índices de ações MSCI (emergentes)            |        |       |
-| Ibovespa                                      |        |       |
+| Ibovespa                                      | ok! | LCA |
--- Scripts/Dados.R
+++ Scripts/Dados.R
@@ -6,7 +6,7 @@ dados1_temp <- read_excel("Dados/bm&f.xlsx", sheet = "Swaps", skip = 13,
                                         "skip", "skip", "skip", "skip", "skip", "skip", "skip",
                                         "skip"))
 
-colnames(dados1_temp) <- c("obs", "juros_360_br") # Não tem o de 5 anos. No BCB a de 360 dias tem no Bacen (código 7806)
+colnames(dados1_temp) <- c("obs", "juros_360d_br") # Não tem o de 5 anos. No BCB a de 360 dias tem no Bacen (código 7806)
 
 dados2_temp <- read_excel("Dados/credit_Default_Swap.xlsx", sheet = "CDS", skip = 11,
                           col_types = c("date", "skip", "skip", "skip", "skip", "skip", "skip",
@@ -22,19 +22,50 @@ dados3_temp <- read_excel("Dados/vix.xlsx", sheet = "VIX", skip = 11,
 
 colnames(dados3_temp) <- c("obs", "vix")
 
-dados4_temp <- read_excel("Dados/Commodity_research_bureau.xlsx", skip = 11, sheet = "CRB Future Spot",
+dados4_temp <- read_excel("Dados/Commodity_research_bureau.xlsx", sheet = "CRB Future Spot", skip = 11,
                           col_types = c("date", "skip", "skip", "skip", "skip", "skip", "skip", "skip",
                                         "numeric", "skip", "skip", "numeric", "skip",
                                         "skip", "skip"))
 
 colnames(dados4_temp) <- c("obs", "crb_metals", "crb_foodstuffs")
 
-dados5_temp <- read_excel("Dados/Commodities_spot.xlsx", skip = 11, sheet = "Energia",
+dados5_temp <- read_excel("Dados/Commodities_spot.xlsx", sheet = "Energia", skip = 11,
                           col_types = c("date", "numeric", "numeric", "skip", "skip", "skip", "skip",
                                         "skip", "skip", "skip", "skip"))
 
 colnames(dados5_temp) <- c("obs", "petro_brent", "petro_wti")
 
+dados6_temp <- read_excel("Dados/Indicadores.xlsx", sheet = "Câmbio - Brasil", skip = 11,
+                          col_types = c("date", "skip", "numeric", "skip", "skip", "skip"))
+
+colnames(dados6_temp) <- c("obs", "cambio")
+
+dados7_temp <- read_excel("Dados/Indicadores.xlsx", sheet = "Bovespa", skip = 11,
+                          col_types = c("date", "numeric", "skip", "skip", "skip"))
+
+colnames(dados7_temp) <- c("obs", "ibovespa")
+
+dados8_temp <- read_excel("Dados/Indicadores.xlsx", sheet = "Treasuries", skip = 11,
+                          col_types = c("date", "skip", "numeric", "skip", "skip", "numeric",
+                                        "skip", "skip", "skip", "numeric", "skip", "skip"))
+
+colnames(dados8_temp) <- c("obs", "juros_us_3m", "juros_us_2a", "juros_us_10a")
+
 #### Conversão para xts ####
 
+dados1_temp <- xts(x = dados1_temp[, -1], order.by = dados1_temp$obs)["2002/"]
+dados2_temp <- xts(x = dados2_temp[, -1], order.by = dados2_temp$obs)["2002/"]
+dados3_temp <- xts(x = dados3_temp[, -1], order.by = dados3_temp$obs)["2002/"]
+dados4_temp <- xts(x = dados4_temp[, -1], order.by = dados4_temp$obs)["2002/"]
+dados5_temp <- xts(x = dados5_temp[, -1], order.by = dados5_temp$obs)["2002/"]
+dados6_temp <- xts(x = dados6_temp[, -1], order.by = dados6_temp$obs)["2002/"]
+dados7_temp <- xts(x = dados7_temp[, -1], order.by = dados7_temp$obs)["2002/"]
+dados8_temp <- xts(x = dados8_temp[, -1], order.by = dados8_temp$obs)["2002/"]
+
 #### Junção dos dados em um único data frame ####
+
+dados <- merge.xts(dados1_temp, dados2_temp, dados3_temp, dados5_temp, dados6_temp, dados7_temp, dados8_temp) # O dados4_temp está dando problema!
+
+#### Limpeza do ambiente ####
+
+remove(list = ls(pattern = "_temp"))
