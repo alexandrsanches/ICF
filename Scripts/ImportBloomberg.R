ff98c019ae97b558870b4b57e8d9d76baf46ddc7
Alexandre Sanches <al.sanches@icloud.com>
2020-06-17 10:13:12 -0300

    [add] Arquivo de importação de dados.

---
+++ Scripts/ImportBloomberg.R
@@ -0,0 +1,88 @@
+### Load packages
+
+library(Rblpapi)
+library(lubridate)
+library(tidyverse)
+library(xts)
+
+loadb <- function(ticker, xts = TRUE) {
+    
+    if(xts == TRUE) {
+        
+        data <- bdh(ticker, c("PX_LAST"), start.date = ymd("2000-01-01"), options = c("periodicitySelection" = "DAILY"))
+        data <- xts(data, order.by = data[,1])
+        data <- data[,-1]    
+        
+    } else if(xts == FALSE) {
+        
+        data <- bdh(ticker, c("PX_LAST"), start.date = ymd("2000-01-01"), options = c("periodicitySelection" = "DAILY"))
+        
+    }
+    
+}
+
+### Load bloomberg terminal
+
+blpConnect()
+
+## Juro Brasil
+({
+juros1a_br <- loadb("BCSWFPD BGN Curncy")
+juros5a_br <- loadb("BCSWNPD BGN Curncy")
+})
+
+## Juro internacional
+
+# EUA
+juros3m_us <- loadb("H15T3M Index")
+juros2a_us <- loadb("H15T2Y Index")
+juros10a_us <- loadb("H15T10Y Index")
+
+# Reino Unido
+juros3m_uk <- loadb("GUKG3M Index")
+juros2a_uk <- loadb("GUKG2 Index")
+juros10a_uk <- loadb("GUKG10 Index")
+
+# Alemanha
+juros3m_de <- loadb("I01603M Index")
+juros2a_de <- loadb("GDBR2 Index")
+juros10a_de <- loadb("GDBR10 Index")
+
+# Japão
+juros3m_jp <- loadb("GJGB3M Index")
+juros2a_jp <- loadb("GJGB2 Index")
+juros10a_jp <- loadb("GJGB10 Index")
+
+
+## Moedas
+({
+cambio <- loadb("BZFXPTAX Index")
+dxy_desenv <- loadb("DXY Curncy")
+dxy_emerg <- loadb("FXJPEMCS Index")
+})
+
+## Commodities
+({
+crb_food <- loadb("CRB FOOD Index")
+crb_metal <- loadb("CRB METL Index")
+})
+
+## Petróleo
+({
+petro_wti <- loadb("CL1 Comdty")
+petro_brent <- loadb("CO1 Comdty")
+})
+
+## Risco
+({
+cds_br <- loadb("CBRZ1U5 CBIN Curncy", xts = F)
+vix <- loadb("VIX Index", xts = F)
+})
+
+## Mercado de capitais
+({
+msci_emerg <- loadb("MXEF Index")
+msci_desenv <- loadb("MXWO Index")
+ibovespa <- loadb("IBOV Index")
+})
+
