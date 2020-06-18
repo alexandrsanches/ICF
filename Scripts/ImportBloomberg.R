0bcb42f7d35a610896d4719982fa2a93c9fbcfea
Alexandre Sanches <al.sanches@icloud.com>
2020-06-18 10:08:03 -0300

    [change] Reordenação de dados em categorias.

--- Scripts/ImportBloomberg.R
+++ Scripts/ImportBloomberg.R
@@ -4,8 +4,9 @@ library(Rblpapi)
 library(lubridate)
 library(tidyverse)
 library(xts)
+library(plyr)
 
-loadb <- function(ticker, xts = TRUE) {
+loadb <- function(ticker) {
     
     data <- bdh(ticker, c("PX_LAST"), start.date = ymd("2000-01-01"), options = c("periodicitySelection" = "DAILY"))
     
@@ -18,14 +19,16 @@ blpConnect()
 ## Juro Brasil
 
 ({
-    juros1a_br <- loadb("BCSWFPD BGN Curncy")
-    juros5a_br <- loadb("BCSWNPD BGN Curncy")
+    juros1a_br <- loadb("BCSFLPDV CMPN Curncy")
+    juros5a_br <- loadb("BCSFSPDV CMPN Curncy")
 })
 
 colnames(juros1a_br) <- c("data", "juro1a_br")
 colnames(juros5a_br) <- c("data", "juro5a_br")
 
-juros_brasil <- merge(juros1a_br, juros5a_br, all = TRUE)
+juroBrasil <- list(juros1a_br, juros5a_br) %>%
+    join_all(by = "data") %>%
+    arrange(data)
 
 ## Juro internacional
 
@@ -73,10 +76,14 @@ colnames(juros3m_jp) <- c("data", "juros3m_jp")
 colnames(juros2a_jp) <- c("data", "juros2a_jp")
 colnames(juros10a_jp) <- c("data", "juros10a_jp")
 
-juros_exterior <- merge(juros3m_us, juros2a_us, juros10a_us,
-                        juros3m_uk, juros2a_uk, juros10a_uk,
-                        juros3m_de, juros2a_de, juros10a_de,
-                        juros3m_jp, juros3m_jp, juros10a_jp)
+juroExterior <- list(juros3m_us, juros2a_us, juros10a_us,
+         juros3m_uk, juros2a_uk, juros10a_uk,
+         juros3m_de, juros2a_de, juros10a_de,
+         juros3m_jp, juros3m_jp, juros10a_jp) %>%
+    join_all(by = "data") %>%
+    arrange(data)
+
+rm(list = ls(pattern = "_"))
 
 ## Risco
 ({
@@ -87,9 +94,14 @@ juros_exterior <- merge(juros3m_us, juros2a_us, juros10a_us,
 colnames(cds_br) <- c("data", "cds_br")
 colnames(vix) <- c("data", "vix")
 
-risco <- merge(cds_br, vix, all = TRUE)
+risco <- list(cds_br, vix) %>%
+    join_all(by = "data") %>%
+    arrange(data)
+
+rm(cds_br, vix)
 
 ## Moedas
+
 ({
     cambio <- loadb("BZFXPTAX Index")
     dxy_desenv <- loadb("DXY Curncy")
@@ -100,7 +112,11 @@ colnames(cambio) <- c("data", "cambio")
 colnames(dxy_desenv) <- c("data", "dxy_desenv")
 colnames(dxy_emerg) <- c("data", "dxy_emerg")
 
-moedas <- merge(cambio, dxy_desenv, dxy_emerg, all = TRUE)
+moedas <- list(cambio, dxy_desenv, dxy_emerg) %>%
+    join_all(by = "data") %>%
+    arrange(data)
+
+rm(cambio, list = ls(pattern = "dxy"))
 
 ## Petróleo
 ({
@@ -111,7 +127,11 @@ moedas <- merge(cambio, dxy_desenv, dxy_emerg, all = TRUE)
 colnames(petro_wti) <- c("data", "petro_wti")
 colnames(petro_brent) <- c("data", "petro_brent")
 
-petroleo <- merge(petro_wti, petro_brent, all = TRUE)
+petroleo <- list(petro_wti, petro_brent) %>%
+    join_all(by = "data") %>%
+    arrange(data)
+
+rm(list = ls(pattern = "_"))
 
 ## Commodities
 ({
@@ -122,7 +142,11 @@ petroleo <- merge(petro_wti, petro_brent, all = TRUE)
 colnames(crb_food) <- c("data", "crb_food")
 colnames(crb_metal) <- c("data", "crb_metal")
 
-commodities <- merge(crb_food, crb_metal, all = TRUE)
+commodities <- list(crb_food, crb_metal) %>% 
+    join_all(by = "data") %>%
+    arrange(data)
+
+rm(list = ls(pattern = "crb"))
 
 ## Mercado de capitais
 ({
@@ -135,5 +159,8 @@ colnames(msci_emerg) <- c("data", "msci_emerg")
 colnames(msci_desenv) <- c("data", "msci_desenv")
 colnames(ibovespa) <- c("data", "ibovespa")
 
-merc_capitais <- merge(msci_desenv, msci_emerg, ibovespa, all = TRUE)
+mercCapitais <- list(msci_emerg, msci_desenv, ibovespa) %>%
+    join_all(by = "data") %>%
+    arrange(data)
 
+rm(ibovespa, list = ls(pattern = "msci"))
